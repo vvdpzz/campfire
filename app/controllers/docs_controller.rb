6a16db0736fd257bc04f2dfafc4bd788a4352757
VvDPzZ <vvdpzz@gmail.com>
2011-07-19 19:45:27 +0800

    上传和删除的同步功能

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,12 +3,23 @@ class DocsController < ApplicationController
     channel = Channel.find params[:channel_id]
     @doc = channel.docs.build(:src => params[:Filedata], :user_id => current_user.id)
     if @doc.save
-      render :json => { :status => 'success', :src => @doc.src.url, :name => File.basename(@doc.src.url) }.to_json
+      
+      filename = File.basename(@doc.src.url)
+      
+      value = { :id => @doc.id, :uid => current_user.id, :src => @doc.src.url, :filename => filename, :name => current_user.username }.to_json
+
+      Pusher["presence-channel_#{params[:channel_id]}"].trigger('doc-create', value)
+      
+      render :json => { :status => 'success', :id => @doc.id, :uid => current_user.id, :src => @doc.src.url, :filename => filename, :name => current_user.username }.to_json
     end
   end
   
   def destroy
     @doc = current_user.docs.find(params[:id])
+    filename = File.basename(@doc.src.url)
+    channel_id = @doc.channel.id
+    value = { :id => @doc.id, :uid => current_user.id, :src => @doc.src.url, :filename => filename, :name => current_user.username }.to_json
     @doc.destroy
+    Pusher["presence-channel_#{channel_id}"].trigger('doc-destroy', value)
   end
 end

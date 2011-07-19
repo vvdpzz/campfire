20a0a3172aaee7fb82197c937a75fe9bb2700fda
VvDPzZ <vvdpzz@gmail.com>
2011-07-19 14:59:04 +0800

    完善上传功能

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,10 +1,14 @@
 class DocsController < ApplicationController
   def create
-    puts "balabalabalabalabalabalabalabalabalabalabalabalabalabalabalabalabala"
     channel = Channel.find params[:channel_id]
     @doc = channel.docs.build(:src => params[:Filedata], :user_id => current_user.id)
     if @doc.save
-      render :json => { :status => 'success', :src => @doc.src.url }.to_json
+      render :json => { :status => 'success', :src => @doc.src.url, :name => File.basename(@doc.src.url) }.to_json
     end
+  end
+  
+  def destroy
+    doc = current_user.docs.find(params[:id])
+    doc.destroy
   end
 end
